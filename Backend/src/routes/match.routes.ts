cbaa04f45084326eef9eda421875c5fa07dd0a23
itsMilanCodesu <milantamang350359@gmail.com>
2026-04-19 16:56:43 +0900

    feature : created get-matches api which gets matches based on status if given if not then it will fetch all matches

--- Backend/src/routes/match.routes.ts
+++ Backend/src/routes/match.routes.ts
@@ -1,9 +1,11 @@
 import express from 'express';
-import { createMatchController } from '../controllers/match.controller.js';
+import { createMatchController, getMatchController } from '../controllers/match.controller.js';
+import { match } from 'assert';
 
 const matchRouter = express.Router();
 
 
 matchRouter.post("/create-match",createMatchController)
+matchRouter.get("/get-match",getMatchController )
 
 export default matchRouter; 
