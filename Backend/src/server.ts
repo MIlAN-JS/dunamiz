1ff9df49ddf49efcbd0501c44d4d62f52c30e91c
itsMilanCodesu <milantamang350359@gmail.com>
2026-04-22 22:25:41 +0900

    feat : added leaving match api , todo : fix needed  in joinmatch api

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,21 +1,20 @@
 import app from "./app.js"
 import config from "./config/config.js"
 import connectToDb from "./config/database.js"
+import {createServer} from "http";
+import { Server } from "socket.io";
 
-
-
-
-
+const server = createServer(app);
 
 
 
 
 //connecting to our database
 connectToDb();
-
+const port = config.PORT || 5000
 // running our app 
-app.listen(config.PORT, ()=>{
-    console.log("our app is running on port " + config.PORT)
+server.listen(8000, ()=>{
+    console.log("our app is running on port " + port)
 })
 
 
